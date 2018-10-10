9ea156976a1658fe3b880f01de2d57ab662cabc0
Khrissian Joi Neuda <34526517+khrissianjoi@users.noreply.github.com>
2018-10-10 15:28:22 +0100

    Update ca268_week3.py

--- ca268_week3.py
+++ ca268_week3.py
@@ -14,7 +14,7 @@ while n:
 			dic[string[0]] = string[1]
 		elif string[0] in dic and string[1] == "?":
 			print("{} has number {}".format(string[0], dic[string[0]]))
-		elif string[0] not in a and string[1] == "?":
+		elif string[0] not in dic and string[1] == "?":
 			print("Sorry, there is no {}".format(string[0]))
 	elif string == "!!":
 		print("Bye")
