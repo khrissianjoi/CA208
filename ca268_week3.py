ac130f7a57c4542d73ef2cca7af962c4c76e905f
Khrissian Joi Neuda <34526517+khrissianjoi@users.noreply.github.com>
2018-10-10 13:50:05 +0100

    Update ca268_week3.py

--- ca268_week3.py
+++ ca268_week3.py
@@ -3,17 +3,17 @@ phone_numbers.py
 print("Enter a name and number, or a name and ? to query (!! to exit)\n", end="")
 
 n = True
-a = {}
+dic = {}
 while n:
 	string = input()
 	if string != "!!" :
 		string = string.split()
-		if string[0] not in a and string[1]!= "?":
-			a[string[0]] = string[1]
-		elif string[0] in a and string[1] != "?":
-			a[string[0]] = string[1]
-		elif string[0] in a and string[1] == "?":
-			print("{} has number {}".format(string[0], a[string[0]]))
+		if string[0] not in dic and string[1]!= "?":
+			dic[string[0]] = string[1]
+		elif string[0] in dic and string[1] != "?":
+			dic[string[0]] = string[1]
+		elif string[0] in dic and string[1] == "?":
+			print("{} has number {}".format(string[0], dic[string[0]]))
 		elif string[0] not in a and string[1] == "?":
 			print("Sorry, there is no {}".format(string[0]))
 	elif string == "!!":
